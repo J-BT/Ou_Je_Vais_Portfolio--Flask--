0863619e754faf2511ca08a31af9cdfaf69f3c6a
Jean-Baptiste <jbt@jbtcl.ubuntuserver>
2021-07-30 14:06:02 +0000

    1er commit DigitalOcean

--- sousdomaine_maDestination.py
+++ sousdomaine_maDestination.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:purple'>Mon sous domaine maDestination!</h1>"
+    return "<h1 style='color:purple'>Mon sous domaine pandasprojects maDestination!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
