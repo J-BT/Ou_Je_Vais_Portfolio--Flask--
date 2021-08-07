b145985aaee78b97b0ed41e68fa41f0cbb58d1c8
J-BT <jeanbaptiste.theroulde@gmail.com>
2021-08-07 17:22:58 +0200

    avion tres grands ecrans OK

--- app/static/js/ouJeVais.js
+++ app/static/js/ouJeVais.js
@@ -15,18 +15,37 @@ document.addEventListener("DOMContentLoaded", function(){
   }); 
 
   /************************  Anine.js J'y vais **************************/
+    
+//Avion
+let avion = anime({
+  targets: '#avionJs',
+  translateX: [
+    {value: [0, 1900], duration: 3000, delay: 1000},
+    {value: [1900, 1750], duration: 2000, delay: 200}
+  ], 
+  translateY: [
+    {value: [0], duration: 800, delay: 1500},
+    {value: [0, -230], duration: 1000, delay: 1500}
+  ],
+  /*translateY: [0, -200],*/
+  /*delay: 2500,*/
+  direction: 'normal',
+  loop: false,
+  easing: 'easeOutSine'
+});
   
+
+  //Lettres
   let animation = anime({
   targets: '.letter',
   opacity: 1,
   translateY: 50, 
   rotate: {
     value: 360,
-    duration: 2000,
+    duration: 1500,
     easing: 'easeInExpo'
   }, 
   scale: anime.stagger([0.7, 1], {from: 'center'}), 
   delay: anime.stagger(100, {start: 1000}), 
   translateX: [-10, 30]
 });   
-  
